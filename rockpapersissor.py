6460ac996b9fa0b6c3f32465b90a1ac8e7c603a4
Smeet Ajudiya <149157156+SmeetAjudiya@users.noreply.github.com>
2024-02-29 14:13:56 +0530

    Add files via upload

---
+++ rockpapersissor.py
@@ -0,0 +1,24 @@
+import random
+game_list = ['r','p','s']
+computer = random.choice(game_list)
+#print(computer)
+while True :
+    user = input("Enter your choice r,p or s (Input anything else to end):")
+
+    if user == computer:
+        print("TIE")
+    elif user == 'r' and computer == 's':
+        print("USER WON")
+    elif user == 'r' and computer == 'p':
+        print("COMPUTER WON")
+    elif user == 'p' and computer == 's':
+        print("COMPUTER WON")
+    elif user == 'p' and computer == 'r':
+        print("USER WON")
+    elif user == 's' and computer == 'r':
+        print("COMOUTER WON")
+    elif user == 's' and computer == 'p':
+        print("USER WON")
+    else:
+        print("GAME ENDS")
+        break
